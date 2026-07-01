f099af5d1050ffd543b114cc4b18e9bd2f71845e
Luis <103071836+LuisAlbertoQ@users.noreply.github.com>
2026-07-01 16:40:18 -0500

    cambiso

--- back_GenApp/geneapp/urls.py
+++ back_GenApp/geneapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path('api/v1/reportes/', include('reportes.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
